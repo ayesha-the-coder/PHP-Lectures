64b530bb4cb5335b4eb2b9c9a662bef8d6f98678
ayesha-the-coder <ayeshafarhat4me@gmail.com>
2025-12-26 10:30:36 -0800

    Lecture-1

---
+++ Lecture1.php
@@ -0,0 +1,13 @@
+<?php
+// Example # 1
+echo "Hello World <br>";
+echo "php stand for " . "Hypertext Preprocessor";
+echo " and also ", "scripting language <br>";
+
+//Varibles Example
+$name = "Ali";
+$age = 21;
+$email = "ali@gmail.com";
+
+echo "My name is " . $name . "<br>Your Age is " .$age. "<br> Your Email is " .$email;
+?>
