b1990e27be8cb163574bf3063ff6e28ba5240fea
ayesha-the-coder <ayeshafarhat4me@gmail.com>
2025-12-26 19:52:12 -0800

    update code

--- Lecture1.php
+++ Lecture1.php
@@ -11,4 +11,26 @@ $email = "ali@gmail.com";
 
 echo "My name is " . $name . "<br>Your Age is " .$age. "<br> Your Email is " .$email. "<br>";
 echo "${name} is a Good Boy Age is ${age} Email is ${email} ";
+
+// Arithmetic Operator Example # 1
+$a = 50;
+$b = 200;
+$c = $a + $b;
+echo "Result is ${c} <br>";
+
+
+// Arithmetic Operator Example # 2
+
+$num1 = 200;
+$num2 = 150;
+// $num3 = $num1 + $num2;
+$num3 = $num1 - $num2;
+// $num3 = $num1 * $num2;
+// $num3 = $num1 / $num2;
+
+
+echo "Addition ${num3}";
+// echo "Subtraction ${num3}";
+// echo "Multiple ${num3}";
+// echo "Divide ${num3}";
 ?>
